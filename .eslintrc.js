decf4de7e4601a69d446daa7ec33f8f13cd8a069
carlosprietobarron <carlosprietobarron@gmail.com>
2020-09-19 19:29:08 -0500

    Add elinter files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,15 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-  },
-  rules: {
-    "no-shadow": "off",
-    "no-param-reassign": "off",
-    "eol-last": "off",
-    "arrow-parens": "off",
-    "linebreak-style": "off",
-    "import/prefer-default-export": "off",
-    "import/no-cycle": "off"
-  },
+    "env": {
+        "browser": true,
+        "es2020": true
+    },
+    "extends": [
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
 };
